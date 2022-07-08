31ce50e342b3914e21399237a8f170c8e27129fe
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-07-08 07:16:22 -0400

    fetch pokemons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  
+  const [loadMore, setLoadMOre] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    console.log(data)
+  }
+
+  useEffect(()=>{
+    getAllPokemons()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <h1>Pokemon</h1>
+      <div className='pokemon-container'>
+        <div className='all-container'>
+
+        </div>
+        <button className='load-more'>Cargar más</button>
+      </div>
     </div>
   );
 }
