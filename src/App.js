2946e7a2f6ed77166e0843f46b8bc65a8c038a9b
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-07-09 22:39:37 -0400

    main structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { BrouserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App () {
 
   return (
-    <div className='app-container'>
+    <div className="App">
+      <h1>PokeFans</h1>
     </div>
   );
 }
