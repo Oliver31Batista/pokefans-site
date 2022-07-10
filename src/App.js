71c4c6b9aeaf291ee8535fd82783cc2c8572875b
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-07-09 22:09:27 -0400

    starting again...

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
 import './App.css';
 
-function App() {
-
-  const [allPokemons, setAllPokemons] = useState([]);
-
-  
-  const [loadMore, setLoadMOre] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
-
-  const getAllPokemons = async () => {
-    const res = await fetch(loadMore)
-    const data = await res.json()
-
-    console.log(data)
-  }
-
-  useEffect(()=>{
-    getAllPokemons()
-  }, [])
+function App () {
 
   return (
     <div className='app-container'>
-      <h1>Pokemon</h1>
-      <div className='pokemon-container'>
-        <div className='all-container'>
-
-        </div>
-        <button className='load-more'>Cargar más</button>
-      </div>
     </div>
   );
 }
