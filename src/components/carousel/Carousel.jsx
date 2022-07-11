fa72a96f58dc3e0e3272f9a37bde9f4994f6e2e7
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-07-10 21:32:09 -0400

    carousel added but need some new images

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import fireTypes from '../imgs/fireTypes.jpg';
-import grassTypes from '../imgs/grassTypes.jpg';
-import waterTypes from '../imgs/waterTypes.jpg';
-import electricTypes from '../imgs/electricTypes.jpg';
-import darkTypes from '../imgs/darkTypes.jpg';
+import "../css/Carousel.css";
+import kyogre from '../imgs/kyogre.png';
+import pokimg1 from '../imgs/pokimg1.webp';
+import pokimg2 from '../imgs/pokimg2.jpg';
+import pokimg3 from '../imgs/pokimg3.jpg';
+import pokimg4 from '../imgs/pokimg4.jpg';
+import pokimg5 from '../imgs/pokimg5.jpg';
+
 
 const Carousel = () => {
   return (
@@ -11,26 +14,31 @@ const Carousel = () => {
         <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 3"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 6"></button>
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src={fireTypes} className="d-block w-700" alt="Pokemons de tipo fuego"/>
+      <img src={kyogre} className="d-block w-700" alt="Pokemons de tipo fuego" width="1280" height="720"/>
     </div>
     <div className="carousel-item">
-      <img src={grassTypes} className="d-block w-700" alt="Pokemons de tipo hierba"/>
+      <img src={pokimg1} className="d-block w-700" alt="Pokemons de tipo hierba" width="1280" height="720"/>
     </div>
     <div className="carousel-item">
-      <img src={waterTypes} className="d-block w-700" alt="Pokemons de tipo agua"/>
+      <img src={pokimg2} className="d-block w-700" alt="Pokemons de tipo agua" width="1280" height="720"/>
     </div>
     <div className="carousel-item active">
-      <img src={electricTypes} className="d-block w-700" alt="Pokemons de tipo electrico"/>
+      <img src={pokimg3} className="d-block w-700" alt="Pokemons de tipo electrico" width="1280" height="720"/>
     </div>
     <div className="carousel-item">
-      <img src={darkTypes} className="d-block w-700" alt="Pokemons de tipo oscuro"/>
+      <img src={pokimg4} className="d-block w-700" alt="Pokemons de tipo oscuro" width="1280" height="720"/>
+    </div>
+    <div className="carousel-item">
+      <img src={pokimg5} className="d-block w-700" alt="Pokemons de tipo x" width="1280" height="720"/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
